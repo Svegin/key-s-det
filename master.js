f03e71e21d5a00f118b9c130e0cc57cc424f4aae
Svegin <svegin@Svegins-Mac-mini.local>
2017-01-06 16:27:21 -0600

    add comments

--- master.js
+++ master.js
@@ -5,8 +5,10 @@ const secretCode = 'morning';
 // listener for keyboard
 window.addEventListener('keyup', (e) => {
   console.log(e.key);
+  // limitation arrow with pressed keys
   pressed.push(e.key);
   pressed.splice(-secretCode.length -1, pressed.length - secretCode.length);
+  // overlaping with secretCode
   if (pressed.join('').includes(secretCode)) {
     console.log('❤️ ❤️ ❤️ ❤️');
     cornify_add();
